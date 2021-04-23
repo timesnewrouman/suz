44175f2add2f53f20378703b7d731857da150231
Zhigaikov Roman <zhigaikov.roman@yandex.ru>
2021-04-23 16:40:19 +0300

    23-04-2021 suz

---
+++ src/components/Table.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    class="elevation-1"
+  >
+    <template v-slot:item.calories="{ item }">
+      <v-chip
+        color="green"
+        dark
+      >
+        {{ item.calories }}
+      </v-chip>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+// :color="getColor(item.calories)"
+// import Vuetify from 'vuetify';
+
+export default {
+  name: 'Table',
+  props: {},
+  // vuetify: new Vuetify(),
+  
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        {text: 'Calories', value: 'calories'},
+        {text: 'Fat (g)', value: 'fat'},
+        {text: 'Carbs (g)', value: 'carbs'},
+        {text: 'Protein (g)', value: 'protein'},
+        {text: 'Iron (%)', value: 'iron'},
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+      ],
+    }
+  },
+  methods: {
+    getColor(calories) {
+      if (calories > 400) {
+        console.log('123');
+        return 'red'
+      } else if (calories > 200) {
+        console.log('456');
+        return 'orange'
+      } else {
+        console.log('789');
+        return 'green'
+      }
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+// import Vuetify from 'vuetify/lib';
+import 'vuetify/dist/vuetify.min.css';
+// import colors from 'vuetify/lib/util/colors'
+import App from './App.vue';
+// import 'vuetify/src/styles/main.sass';
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify: new Vuetify({
+    theme: {
+      green: '#4CAF50',
+      // themes: {
+      //   light: {
+      //     red: '#F44336',
+      //     orange: '#FF9800',
+      //     green: '#4CAF50',
+      //   },
+      // },
+    },
+  }),
+//  el: '#app',
   render: h => h(App),
 }).$mount('#app')
